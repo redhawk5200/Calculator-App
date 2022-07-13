c09698d0840f91d062556298c8823607cbe3d3dd
Abdul Wahab Khan <abdulwahab052001@gmail.com>
2022-07-13 16:39:22 +0500

    useState

--- App.js
+++ App.js
@@ -1,25 +1,33 @@
 //to change a data type dynamically you have to import {useState} from react library
-import React,{useState} from 'react';
+import React, {useState} from 'react';
 
-
-//here we import the properties like text, buttons etc from react native 
+//here we import the properties like text, buttons etc from react native
 import {StyleSheet, Text, View, StatusBar, Button} from 'react-native';
 
 export default function App() {
-//now to assign the dynamic variabe you define the variable it will be assigned to alongside with the method from the react library that you imported.
-  const [name,setName] = useState("Abdul Wahab")
+  const [name, setName] = useState('This is the old name');
+  const [person, setPerson] = useState({name: 'Abdur Rafay', age: 20});
 
-//creating a function changeme to run when user clicks the button
-const clickme =() => {
-  setName('Wow');
-}
+  //creating a function changeme to run when user clicks the button
+  const clickme = () => {
+    setName('This is the new name');
+    setPerson({name: 'Abdul Wahab', age: 22});
+  };
 
   return (
     <View style={styles.container}>
       <Text>{name}</Text>
+      <Text>
+        My name is {person.name} and age is {person.age}
+      </Text>
       <View style={styles.button}>
         {/*the button will initiate the method clickme when the user will click it using the property onPress of the button*/}
-      <Button title='change' color='green' onPress={clickme}></Button> 
+        <Button
+          title="change"
+          color="green"
+          onPress={clickme}
+          touchSoundDisabled={false}
+        />
       </View>
     </View>
   );
@@ -35,10 +43,9 @@ const styles = StyleSheet.create({
   button: {
     backgroundColor: 'red',
     padding: 10,
-    margin: 20
-  }
+    margin: 20,
+  },
 });
-
 
 //<Text>Hello world kadnoasoasdajoidasj</Text>
 //<StatusBar style="auto" />j
