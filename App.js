132f636bdb91eda6bd5fe5757e67ca6934a4c8c8
Abdul Wahab Khan <abdulwahab052001@gmail.com>
2022-07-14 11:30:25 +0500

    External Styling

--- App.js
+++ App.js
@@ -10,65 +10,49 @@ import {
   Button,
   ScrollView,
   FlatList,
+  TouchableOpacity,
 } from 'react-native';
 
+import styles from './styles';
+
 export default function App() {
-  const [fruit, setFruit] = useState([
-    {name: 'Apple', key: 1},
-    {name: 'Mango', key: 2},
-    {name: 'Banana', key: 3},
-    {name: 'Orange', key: 4},
-    {name: 'Mango', key: 5},
-    {name: 'Apple', key: 6},
-    {name: 'Mango', key: 7},
-    {name: 'Banana', key: 8},
-    {name: 'Orange', key: 9},
-    {name: 'Mango', key: 10},
-    {name: 'Apple', key: 11},
-    {name: 'Mango', key: 12},
-    {name: 'Banana', key: 13},
-    {name: 'Orange', key: 14},
-    {name: 'Mango', key: 15},
+  const [people, setPeople] = useState([
+    {name: 'Vincius Jr', key: 1},
+    {name: 'Mendy', key: 2},
+    {name: 'Karim Benzema', key: 3},
+    {name: 'Carvajal', key: 4},
+    {name: 'Eder Militao', key: 5},
+    {name: 'David Alaba', key: 6},
+    {name: 'Rodrygooo', key: 7},
+    {name: 'Luka Modric', key: 8},
+    {name: 'Cavaminga', key: 9},
+    {name: 'Toni Kroos', key: 10},
+    {name: 'Valverde', key: 11},
+    {name: 'Lucas Vasquez', key: 12},
+    {name: 'Nacho', key: 13},
+    {name: 'Dani Ceballos', key: 14},
   ]);
+
+  const pressHandler = name => {
+    console.log(name);
+  }
+
   return (
     <View style={styles.container}>
       <FlatList
-        data={fruit}
-        renderItem={({item})=> (
+        data={people}
+        renderItem={({item}) => (
           <View>
-            <Text style={styles.item}>{item.name}</Text>
+            <TouchableOpacity onPress={() => pressHandler(item.name)}>
+              <Text style={styles.item}>{item.name}</Text>
+            </TouchableOpacity>
           </View>
         )}
       />
-      {/*<ScrollView>
-        {fruit.map(item => {
-          return (
-            <View key={item.key}>
-              <Text style={styles.item}>{item.name}</Text>
-            </View>
-          );
-        })}
-      </ScrollView>*/}
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'white',
-    flex: 1,
-  },
-  button: {
-    padding: 10,
-    margin: 20,
-  },
-  item: {
-    fontSize: 22,
-    backgroundColor: 'yellow',
-    marginTop: 20,
-    padding: 20,
-  },
-});
 
 //<Text>Hello world kadnoasoasdajoidasj</Text>
 //<StatusBar style="auto" />j
---
+++ styles.js
@@ -0,0 +1,18 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+      backgroundColor: 'white',
+      flex: 1,
+    },
+    button: {
+      padding: 10,
+      margin: 20,
+    },
+    item: {
+      fontSize: 22,
+      backgroundColor: 'yellow',
+      marginTop: 20,
+      padding: 20,
+    },
+  });
