2d25542043a702697501c6b504339cacaf8fbdb7
Abdul Wahab Khan <abdulwahab052001@gmail.com>
2022-07-15 11:47:38 +0500

    Dismissing keyboard when user clicks outside textbox.
    Using TouchableWithoutFeedback() and Keyboard()

--- App.js
+++ App.js
@@ -13,40 +13,22 @@ import {
   TouchableOpacity,
   Alert,
   Image,
+  TouchableWithoutFeedback,
+  Keyboard,
 } from 'react-native';
 
 import styles from './styles';
 
 export default function App() {
-  const [name, setName] = useState("");
-  const [age, setAge] = useState(0);
-  const [gender, setGender] = useState("");
-
-  const noHandler = () =>{
-    setName("");
-    setAge(0);
-    setGender("");
-  }
-
-  const pressHandler = () =>{
-    //An alert is generated with the dialogue down below 
-    Alert.alert("Details","Are you sure you want submit?",[
-      //on Pressing yes the console outputs Yes is pressed
-      {text: "Yes", onPress: () => console.log("Yes is pressed")},
-      //on Pressing no the console initiates noHandler method which initializes all the values to default state
-      {text: "No", onPress: () => noHandler()},
-    ])
-  }
-
+  
   return (
-    <View style={styles.container}>
-      <TextInput  style={styles.input} onChangeText={(text)=>setName(text)}  placeholder="Enter your name"/>
-      <TextInput  style={styles.input} onChangeText={(text)=>setAge(text)}  placeholder="Enter your age"/>
-      <TextInput  style={styles.input} onChangeText={(text)=>setGender(text)}  placeholder="Enter your gender"/>
-        {/*on press of the button Submit the pressHandler() method is initiated*/}      
-      <Button title="Submit" style={styles.button} onPress={()=>pressHandler()}/>
-      <Text> Your name is {name}, your age is {age} and your gender is {gender}</Text>
-    </View>
+    <TouchableWithoutFeedback onPress={()=>{
+      Keyboard.dismiss();
+    }}>
+      <View style={styles.container}>
+        <TextInput style={styles.input} placeholder="Enter your name:"/>
+      </View>
+    </TouchableWithoutFeedback>  
   );
 }
 
